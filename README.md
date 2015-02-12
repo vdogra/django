d52d07237f563cbf0db19823b3a49ab7e60dc056
vdogra <vivek@credr.in>
2015-02-12 16:52:43 +0530

    proxy model concept implemented.

---
+++ README.md
@@ -0,0 +1,7 @@
+This Repo contains different Django projects implementing different concepts of Django.
+cd into the particular project and run it as explained in the project's readme to understand the implementation
+of the concept.
+
+#proxymodel
+Explains the concept of proxymodel. The proxy model is used if we just have some behavioral changes to be made to our
+model. By behavioral, I mean if we need to have custom managers or custom model methods, ordering etc.
---
+++ proxymodel/onboarding/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Proxy model example for a normal model
+class Person(models.Model):
+    class Meta:
+        db_table = 'person_info'
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class MyPerson(Person):
+    class Meta:
+        proxy = True
+
+    def __str__(self):
+        return self.first_name
+
+    def get_full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+# Proxy model example for a normal model ENDS
