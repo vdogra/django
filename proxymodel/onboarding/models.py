33f23ec97b73de8c85ee606b13195def9076ad84
vdogra <vivek@credr.in>
2015-02-12 19:23:41 +0530

    adding custom manager using proxy model.

--- proxymodel/onboarding/models.py
+++ proxymodel/onboarding/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+
+#Custom model managers
+
+class StartsWithVManager(models.Manager):
+
+    def get_queryset(self):
+        return super(StartsWithVManager, self).get_queryset().filter(first_name__startswith='V')
+
 # Proxy model example for a custom model
 class Person(models.Model):
     class Meta:
@@ -23,4 +31,11 @@ class OrderedPersonFirstName(Person):
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
+class StartsWithVPerson(Person):
+    class Meta:
+        proxy = True
+
+    objects = models.Manager()
+    starts_with_v_objects = StartsWithVManager()
 # Proxy model example for a normal model ENDS
+
