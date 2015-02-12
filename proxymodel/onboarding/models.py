5834dca02edc106956b641c52445b652eb61bba9
vdogra <vivek@credr.in>
2015-02-12 17:29:50 +0530

    added new proxy model OrderedPersonFirstName which gives persons ordered with first_name

--- proxymodel/onboarding/models.py
+++ proxymodel/onboarding/models.py
@@ -7,15 +7,20 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.first_name
+
 class MyPerson(Person):
     class Meta:
         ordering = ['last_name']
         proxy = True
 
-    def __str__(self):
-        return self.first_name
+class OrderedPersonFirstName(Person):
+    class Meta:
+        ordering = ['first_name']
+        proxy = True
 
-    def get_full_name(self):
+    def __str__(self):
         return self.first_name + ' ' + self.last_name
 
 # Proxy model example for a normal model ENDS
